20fdf858982ebbf88e9ca4fd4510984e29d8dd2d
Vasileios Psychas <vaspsych@gmail.com>
2020-07-07 11:06:48 +0200

    Fixed error handling

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -3,13 +3,13 @@ var createError = require('http-errors');
 exports.handleErrorPaths = (req, res, next) => {
   const error = createError(
     404,
-    `There is not a route '${req.url}' my friend here`
+    `There is not a route called '${req.url}'`
   );
 
   next(error);
 };
 
-exports.errorHandling = (req, res, next) => {
+exports.errorHandling = (err, req, res, next) => {
   const errCode = err.status || 500;
 
   res.status(errCode).send({
